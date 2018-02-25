280350714a500cc22f6c6f24d75f2414ba445168
Kimi-ts <r-g@tut.by>
2018-02-25 11:28:49 +0300

    added https certificate

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Razor_VS_Code_test
 {
@@ -14,12 +16,39 @@ namespace Razor_VS_Code_test
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
-        }
-
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddEnvironmentVariables()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("certificate.json", optional: false, reloadOnChange: true)
+                .AddJsonFile("hosting.json", optional: false, reloadOnChange: true)
                 .Build();
+
+            var certificateSettings = config.GetSection("certificateSettings");
+            string certificateFileName = certificateSettings.GetValue<string>("filename");
+            string certificatePassword = certificateSettings.GetValue<string>("password");
+
+            var certificate = new X509Certificate2(certificateFileName, certificatePassword);
+
+            var host = new WebHostBuilder()
+                .UseKestrel(
+                    options =>
+                    {
+                        options.AddServerHeader = false;
+                        options.Listen(IPAddress.Loopback, 44363, listenOptions =>
+                        {
+                            listenOptions.UseHttps(certificate);
+                        });
+                    }
+                )
+                .UseConfiguration(config)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .UseUrls("https://localhost:44363")
+                .Build();
+
+            host.Run();
+        }
     }
 }

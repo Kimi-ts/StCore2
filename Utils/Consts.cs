d642d68a32f327e8e09dca15e71f64774e8a9523
Kimi-ts <r-g@tut.by>
2018-04-07 14:39:22 +0300

    add user's title icon in chat window

---
+++ Utils/Consts.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Razor_VS_Code_test.Utils
+{
+    public static class Consts
+    {
+        public static string IconPlaceholderUrl = "http://placehold.it/50/55C1E7/fff&text=";   
+    }
+}
